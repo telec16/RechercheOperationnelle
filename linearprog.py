bac6fcddc80204896de2e069b2158c3d305ab2d7
telec <>
2020-01-27 12:14:38 +0100

    improved linear programing

--- linearprog.py
+++ linearprog.py
@@ -8,11 +8,13 @@ def get_pivot(data: List[List[float]]) -> Tuple[int, int]:
     :param data: the matrix to solve
     :return: the pivot point (x, y)
     """
-    # Retrieve the first non-nul negative coefficient on the last line
+    # Retrieve the minimum non-nul negative coefficient on the last line
     y = None
+    c = 0
     for i in range(len(data[-1]) - 1):
-        if data[-1][i] < 0 and y is None:
+        if data[-1][i] < c:
             y = i
+            c = data[-1][i]
 
     # Retrieve the minimum (positive) second member/pivot ratio
     min_ratio = None
@@ -58,14 +60,22 @@ def divide_line(data: List[List[float]], x: int, y: int) -> None:
 
 
 if __name__ == "__main__":
-    data = [[2, 4, 1, 2, 1, 0, 0, 0, 0, 45],
+    data1 = [[2, 4, 1, 2, 1, 0, 0, 0, 0, 45],
             [4, 10, 0, 0, 0, 1, 0, 0, 0, 70],
             [1, 0, 4, 5, 0, 0, 1, 0, 0, 40],
             [0, 0, 5, 10, 0, 0, 0, 1, 0, 70],
             [-.3, -.4, -.5, -.8, 0, 0, 0, 0, 1, 0]]
+    data2 = [[-1, 4, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+            [5, 4, 3, 0, -1, 0, 1, 0, 0, 0, 4],
+            [0, 1, 2, 1, 0, 0, 0, 1, 0, 0, 2],
+            [1, -2, -1, 1, 1, 0, 0, 0, 1, 0, 3],
+            [-1, -4, 2, 0, -10, 0, 0, 0, 0, 1, 0]]
+
+    data = data1
 
     while not all([d >= 0 for d in data[-1]]):
         pivot = get_pivot(data)
+        print(pivot)
         subtract_lines(data, *pivot)
         divide_line(data, *pivot)
 
